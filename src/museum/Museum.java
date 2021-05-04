7a329588787a1b65b1d01d83902e2447f34f2d68
raihansatar <raihanroy25@gmail.com>
2021-05-04 17:32:08 +0800

    latest

--- src/museum/Museum.java
+++ src/museum/Museum.java
@@ -47,7 +47,7 @@ public class Museum {
 			}
 		}
 		
-		int turnstiles = rand.nextInt(4) + 1; // set random turnstiles for wisitors to enter the museum
+		int turnstiles = rand.nextInt(4); // set random turnstiles for wisitors to enter the museum
 		
 		while(SET[turnstiles] == true) { // if true means that turnstile is currently in used
 			try {
@@ -82,7 +82,7 @@ public class Museum {
             }
 		}
 		
-		int turnstiles = rand.nextInt(4) + 1;
+		int turnstiles = rand.nextInt(4);
 		while(NET[turnstiles] == true) {
 	        try {
 	            System.out.println("Waiting... Queue turnstiles NET" + turnstiles);
@@ -103,7 +103,7 @@ public class Museum {
 	
 	// same with above
 	public synchronized void exitEast(Ticket ticket) {
-		int turnstiles = rand.nextInt(4) + 1;
+		int turnstiles = rand.nextInt(4);
 		
 		while(EET[turnstiles] == true) {
 			try {
@@ -127,7 +127,7 @@ public class Museum {
 	
 	// same with above
 	public synchronized void exitWest(Ticket ticket) {
-		int turnstiles = rand.nextInt(4) + 1;
+		int turnstiles = rand.nextInt(4);
 		while(WET[turnstiles] == true) {
 			try {
 				System.out.println("Waiting... Queue turnstiles EET" + turnstiles);
