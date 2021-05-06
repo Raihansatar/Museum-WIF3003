5140bb425b6bfc5c10ccd7be2a4bc7ab38fa0eb5
ajwad_alias <ajwad.alias@gmail.com>
2021-05-06 14:30:57 +0800

    manipulate the timer abit

--- src/museum/Timer.java
+++ src/museum/Timer.java
@@ -10,6 +10,9 @@ public class Timer implements Runnable{
     private int minutes;
     private int startTimeInSec;
     
+    private int timerSpeed = 1;
+    private int threadSleep = 5;
+    
     public Timer(int hour, int minutes){
 
         this.hour = hour*3600;
@@ -23,8 +26,8 @@ public class Timer implements Runnable{
         //Stops by 6:30pm
         while(timer.get()<=67800){
             try{
-                Thread.sleep(50);
-                timer.addAndGet(30);
+                Thread.sleep(this.threadSleep);
+                timer.addAndGet(this.timerSpeed);
                 if(timer.get() == 67800){
                     System.out.println("Time is 6:00pm, museum is now closed");
                 }
