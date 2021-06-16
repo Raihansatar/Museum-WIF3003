97b7074c6687a6e7af08555063a07b89f035b828
raihansatar <raihanroy25@gmail.com>
2021-06-16 22:30:23 +0800

    fixed idk what to call

--- src/museum/Visitor.java
+++ src/museum/Visitor.java
@@ -23,6 +23,7 @@ public class Visitor implements Runnable{
 	public void run() {
 		
 		boolean enter = false;
+		int currentEnterTime = 0;
 		//while museum is not actually closed
 		while(!museum.getTimer().isMuseumActuallyClose()){
 			
@@ -39,6 +40,7 @@ public class Visitor implements Runnable{
 	                     this.museum.enterSouth(this.ticket, staying/60);
 	                }
 	                enter = true;
+	                currentEnterTime = museum.getTimer().getTime();
 	            }
 
             }
@@ -47,7 +49,10 @@ public class Visitor implements Runnable{
 			// if already enter
 			// and timestamp + staying time < current time      (finished visiting)
 			// or museum is closed
-			if( ((enter == true) && ((this.ticket.getTimestamp()+this.staying) <= museum.getTimer().getTime())) || (museum.getTimer().getTime()>=museum.getTimer().getMuseumClosingTime())) {
+			
+			int exitTime = currentEnterTime + this.staying;
+			if( ((enter == true) && ((exitTime) <= museum.getTimer().getTime())) 
+					|| (museum.getTimer().getTime() >= museum.getTimer().getMuseumClosingTime())) {
 				
 				int random = rand.nextInt(2); // randomly assigned gate to visitor to exit
 				if(random == 0) {
